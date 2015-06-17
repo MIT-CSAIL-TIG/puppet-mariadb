1578cd5b7ea4c2f12f6f206afefa6124ba97f30f
Jonathan Proulx <jon@jonproulx.com>
2015-06-17 14:00:54 -0400

    set Ubuntu mirror to NYC (should parameterisze)

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -2,7 +2,7 @@ class mariadb::repo::debian {
   $version = $mariadb::repo_version
 
   apt::source { 'mariadb':
-    location => "http://mirror.aarnet.edu.au/pub/MariaDB/repo/${version}/ubuntu",
+    location => "http://nyc2.mirrors.digitalocean.com/mariadb/repo/${version}/ubuntu",
     repos    => 'main',
   }
 
